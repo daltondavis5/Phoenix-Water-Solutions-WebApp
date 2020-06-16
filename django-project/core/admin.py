203aee6cdb2980015ddf3d31f56b0ef1f0f32678
Gourav Agrawal (Student) <gourav.agrawal10041996@gmail.com>
2020-06-15 17:04:47 -0700

    Added UtilityProvider Models

--- django-project/core/admin.py
+++ django-project/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models.utilityprovider import *
 
-# Register your models here.
+admin.site.register(Provider)
+admin.site.register(Utility)
+admin.site.register(UtilityProvider)
+admin.site.register(Location)
---
+++ django-project/core/models/utilityprovider.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Utility(models.Model):
+    utility_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.utility_type
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=50)
+    utility = models.ManyToManyField(Utility, through='UtilityProvider')
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.city + ", " + self.state
+
+
+class UtilityProvider(models.Model):
+    utility = models.ForeignKey(Utility, on_delete=models.CASCADE)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    unit_measurement = models.FloatField()
