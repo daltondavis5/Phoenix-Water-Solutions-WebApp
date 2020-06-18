f3de16279644d844e5f879f13c1513bfb3aabade
Dalton Davis <daltonndavis5@gmail.com>
2020-06-18 14:48:29 -0700

    Set DEBUG to True so that gunicorn does not run in the pre-staging env

--- django-project/config/settings/staging.py
+++ django-project/config/settings/staging.py
@@ -1,9 +1,3 @@
 from .base import *
 
 #TODO: set ALLOWED_HOSTS
-
-print(" ".join(os.environ.get('SECRET_KEY')))
-print(" ".join(os.environ.get('DB_HOST')))
-print(" ".join(os.environ.get('DB_NAME')))
-print(" ".join(os.environ.get('DB_USER')))
-print(" ".join(os.environ.get('DB_PASS')))
--- scripts/entrypoint.staging.sh
+++ scripts/entrypoint.staging.sh
@@ -8,4 +8,10 @@ python manage.py collectstatic --noinput
 python manage.py wait_for_db
 python manage.py migrate
 
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+if [ "$DEBUG" == "False" ]
+then
+    gunicorn config.wsgi:application --bind 0.0.0.0:8000
+else
+    python manage.py test
+    echo "Did not launch gunicorn becuase DEBUG is True"
+fi
