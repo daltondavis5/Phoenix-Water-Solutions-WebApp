fa105e3e1c14ad197fdbe8634e07a4d1a75779fa
SSRaghavan13 <ssraghavan25@gmail.com>
2020-06-23 08:39:15 -0700

    UtilityProvider card component and migrations file

---
+++ django-project/frontend/src/components/dashboard/provider/UtilityProviderCard.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class UtilityProviderCard extends Component {
+  render() {
+    const {
+      city,
+      state,
+      unit_measurement,
+      utility_type,
+    } = this.props.utility_provider_item;
+    return (
+      <div style={{ marginBottom: "20px" }}>
+        <div className="edit-save-buttons" style={{ height: "40px" }}>
+          <button
+            type="submit"
+            className="btn btn-primary float-right"
+            style={{ marginLeft: "10px", width: "60px" }}
+            onClick={this.props.editButton}
+          >
+            Edit
+          </button>
+        </div>
+        <div className="card">
+          <div className="card-body">Utility: {utility_type}</div>
+          <div className="card-body">State: {state}</div>
+          <div className="card-body">City: {city}</div>
+          <div className="card-body">Unit Measurement: {unit_measurement}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UtilityProviderCard;
