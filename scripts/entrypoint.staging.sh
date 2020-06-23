91bb96f6d2e44b158a38ed58dd045fa116be082f
Dalton Davis <daltonndavis5@gmail.com>
2020-06-23 10:35:16 -0700

    Relocate the migrate command so that it is run in the pre-staging step

--- scripts/entrypoint.staging.sh
+++ scripts/entrypoint.staging.sh
@@ -6,12 +6,12 @@ set -e
 python manage.py collectstatic --noinput
 
 python manage.py wait_for_db
-python manage.py migrate
 
 if [ "$DEBUG" == "False" ]
 then
     gunicorn config.wsgi:application --bind 0.0.0.0:8000
 else
+    python manage.py migrate --noinput
     python manage.py test
     echo "Did not launch gunicorn becuase DEBUG is True"
 fi
