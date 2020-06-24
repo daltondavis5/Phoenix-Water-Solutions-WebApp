1b68e1ce5411aa733f63de5481585adbea5beb7c
devansh <devanshs212@gmail.com>
2020-06-24 11:39:35 -0700

    changes for custom validation

--- django-project/main/serializers/utilityprovider.py
+++ django-project/main/serializers/utilityprovider.py
@@ -27,20 +27,47 @@ class UtilityProviderSerializer(serializers.ModelSerializer):
                   'state', 'unit_measurement']
         read_only_fields = ['provider_name', 'utility_type', 'city', 'state']
 
+    def validate(self, data):
+        instance = self.instance
+
+        provider_name = data.get('provider').get('name')
+        utility_type = data.get('utility').get('type')
+        state = data.get('location').get('state')
+        city = data.get('location').get('city')
+
+        # if utility_provider objects exists in db (Update)
+        if instance is not None:
+            if instance.provider.name != provider_name:
+                raise serializers.ValidationError("Not allowed to change provider name. ")
+
+            if instance.utility.type != utility_type:
+                raise serializers.ValidationError("Not allowed to change utility type. ")
+
+            if instance.location.city != city:
+                raise serializers.ValidationError("Not allowed to change city. ")
+
+            if instance.location.state != state:
+                raise serializers.ValidationError("Not allowed to change state. ")
+            return data
+
+        # if utility_provider does not exist (Create)
+        else:
+            if not Provider.objects.filter(name=provider_name).exists():
+                raise serializers.ValidationError("No provider found in database. ")
+
+            if not Utility.objects.filter(type=utility_type).exists():
+                raise serializers.ValidationError("No utility found in database. ")
+
+            if not Location.objects.filter(state=state, city=city).exists():
+                raise serializers.ValidationError("No location found in database. ")
+            return data
+
+
     def create(self, validated_data):
         provider_name = validated_data.get('provider').get('name')
         utility_type = validated_data.get('utility').get('type')
         state = validated_data.get('location').get('state')
         city = validated_data.get('location').get('city')
-
-        if not Provider.objects.filter(name=provider_name).exists():
-            raise serializers.ValidationError("No provider found in database. ")
-
-        if not Utility.objects.filter(type=utility_type).exists():
-            raise serializers.ValidationError("No utility found in database. ")
-
-        if not Location.objects.filter(state=state, city=city).exists():
-            raise serializers.ValidationError("No location found in database. ")
 
         provider_obj = Provider.objects.get(name=provider_name)
         utility_obj = Utility.objects.get(type=utility_type)
