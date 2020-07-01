9b06104b65f14fbf3b2d91a36b07d2d51fa1c022
SSRaghavan13 <ssraghavan25@gmail.com>
2020-07-01 10:56:25 -0700

    Meter page. Adding editing meters

--- django-project/frontend/src/components/dashboard/units/UnitDetails.js
+++ django-project/frontend/src/components/dashboard/units/UnitDetails.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { createMessage, returnErrors } from "../../../actions/messages";
 import PropTypes from "prop-types";
+import UnitMeters from "./UnitMeters";
 
 export class UnitDetails extends Component {
   static propTypes = {
@@ -12,6 +13,7 @@ export class UnitDetails extends Component {
 
   state = {
     unit: {},
+    meters: [],
   };
 
   componentDidMount() {
@@ -25,10 +27,120 @@ export class UnitDetails extends Component {
       .catch((err) => {
         this.props.returnErrors(err.response.data, err.response.status);
       });
+
+    axios
+      .get(`/api/unit/${this.props.match.params.id}/meters`)
+      .then((response) => {
+        var alteredData = response.data.map((data) => {
+          data.mode = "viewing";
+          return data;
+        });
+        this.setState({
+          meters: alteredData,
+        });
+      })
+      .catch((err) => {
+        this.props.returnErrors(err.response.data, err.response.status);
+      });
   }
 
+  changeToEdit = (index) => () => {
+    let meters = [...this.state.meters];
+    meters[index]["mode"] = "editing";
+    this.setState({
+      meters,
+    });
+  };
+
+  handleChange = (index) => (e) => {
+    let meters = [...this.state.meters];
+    meters[index][e.target.name] = e.target.value;
+    this.setState({
+      meters,
+    });
+  };
+
+  saveButton = (index) => {
+    let meter = this.state.meters[index];
+    const body = {
+      name: meter.name,
+      utility_type: meter.utility_type,
+      installed_date: meter.installed_date,
+      uninstalled_date: meter.uninstalled_date,
+      unit_name: meter.unit_name,
+    };
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    if (meter.mode == "adding") {
+      axios
+        .post("/api/meter/", JSON.stringify(body), config)
+        .then((response) => {
+          meter["id"] = response.data.id;
+          this.props.createMessage({ msg: "Success!" });
+        })
+        .catch((err) => {
+          this.props.returnErrors(err.response.data, err.response.status);
+        });
+    }
+    if (meter.mode == "editing") {
+      axios
+        .put(`/api/meter/${meter["id"]}/`, JSON.stringify(body), config)
+        .then((response) => {
+          this.props.createMessage({ msg: "Success!" });
+        })
+        .catch((err) => {
+          this.props.returnErrors(err.response.data, err.response.status);
+        });
+    }
+    let meters = [...this.state.meters];
+    meters[index]["mode"] = "viewing";
+    this.setState({
+      meter: meters,
+    });
+  };
+
+  deleteButton = (index) => {
+    let meter = this.state.meters[index];
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (meter.mode == "editing") {
+      axios.delete(`/api/meter/${meter["id"]}/`, config).then((response) => {});
+    }
+    let meters = [
+      ...this.state.meters.slice(0, index),
+      ...this.state.meters.slice(index + 1),
+    ];
+    this.setState({
+      meters,
+    });
+  };
+
+  addMeter = (e) => {
+    e.preventDefault();
+    let meters = this.state.meters.concat([
+      {
+        installed_date: "",
+        mode: "adding",
+        name: "",
+        uninstalled_date: "",
+        unit_name: this.state.unit.name,
+        utility_type: "",
+      },
+    ]);
+    this.setState({
+      meters,
+    });
+  };
+
   render() {
-    const { id, name, property } = this.state.unit;
+    const { id, name } = this.state.unit;
     return (
       <>
         <h2 className="text-center mt-3">{name}</h2>
@@ -92,7 +204,15 @@ export class UnitDetails extends Component {
                   role="tabpanel"
                   aria-labelledby="v-pills-meters-tab"
                 >
-                  Meter Details
+                  <UnitMeters
+                    meters={this.state.meters}
+                    unit_id={id}
+                    changeToEdit={this.changeToEdit}
+                    onChange={this.handleChange}
+                    saveButton={this.saveButton}
+                    deleteButton={this.deleteButton}
+                    addMeter={this.addMeter}
+                  />
                 </div>
                 <div
                   className="tab-pane fade"
