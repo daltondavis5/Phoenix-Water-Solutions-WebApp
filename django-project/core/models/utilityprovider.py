8e180c8e87b010e8b4d4073459a83c23f9ea0430
ankit94 <ankit220694@gmail.com>
2020-07-01 15:20:11 -0700

    Added: New APIs for listing meters Changed: Utility model for primary key

--- django-project/core/models/utilityprovider.py
+++ django-project/core/models/utilityprovider.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Utility(models.Model):
-    type = models.CharField(max_length=50, unique=True)
+    type = models.CharField(max_length=50, primary_key=True)
 
     def __str__(self):
         return self.type
