dd85a3ba12414753941a35c2412f7a0a66a872df
Dalton Davis <daltonndavis5@gmail.com>
2020-07-02 09:58:50 -0700

    Last changes to merge into staging

--- scripts/entrypoint.local.sh
+++ scripts/entrypoint.local.sh
@@ -4,7 +4,7 @@
 set -e
 
 python manage.py wait_for_db
-python manage.py makemigrations
+# python manage.py makemigrations
 python manage.py migrate
 python manage.py test
 python manage.py runserver 0.0.0.0:8000
