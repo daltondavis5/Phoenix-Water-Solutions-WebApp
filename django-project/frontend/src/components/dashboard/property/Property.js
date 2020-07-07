41b12db78c7ba4fc6a5731ccdd47953b8227a5d9
Gourav Agrawal (Student) <gourav.agrawal10041996@gmail.com>
2020-07-06 19:29:43 -0700

    Updated PWS logo, homepage look and feel, added tenants dashboard

--- django-project/frontend/src/components/dashboard/property/Property.js
+++ django-project/frontend/src/components/dashboard/property/Property.js
@@ -31,13 +31,15 @@ export class Property extends Component {
           className="jumbotron mt-3 rounded shadow"
           style={{
             padding: "30px",
+            backgroundImage:
+              "radial-gradient( circle 592px at 48.2% 50%,  rgba(255,255,249,0.6) 0%, rgba(160,199,254,1) 74.6% )",
           }}
         >
           <h1 className="text-center mb-4">Property List</h1>
           <ul className="list-group">{propertyItems}</ul>
-          <div className="btn btn-primary mt-4 rounded">
+          <div className="btn btn-primary mt-4 rounded d-flex justify-content-center">
             <Link
-              className="text-light text-decoration-none"
+              className="text-white text-decoration-none"
               to="/property/add"
             >
               Add Property
