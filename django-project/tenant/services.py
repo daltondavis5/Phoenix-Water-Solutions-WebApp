95b57bd19ecc00e4b99bac9ba5ada887a5df4889
SSRaghavan13 <ssraghavan25@gmail.com>
2020-07-08 17:06:52 -0700

    Corrections to test cases

--- django-project/tenant/services.py
+++ django-project/tenant/services.py
@@ -1,3 +1,4 @@
+from core.models.property import Unit
 from core.models.tenant import Tenant, TenantCharge, Payment
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
@@ -10,10 +11,13 @@ def get_tenants_for_unit(unit_id):
     :return queryset: all tenants for a unit
     """
     try:
-        queryset = Tenant.objects.filter(unit=unit_id)
+        unit = Unit.objects.get(pk=unit_id)
+        queryset = Tenant.objects.filter(unit=unit)
         return queryset
-    except (ObjectDoesNotExist, ValueError):
-        return None
+    except (ObjectDoesNotExist):
+        return Exception("Enter a valid ID")
+    except(ValueError):
+        return Exception("Enter a numerical value for ID")
 
 
 def get_current_balance_for_tenant(tenant_id):
@@ -70,8 +74,8 @@ def get_tenant_usage_info(tenant_id):
         tenant = Tenant.objects.get(pk=tenant_id)
         curr_balance = get_current_balance_for_tenant(tenant.id)
         overdue_balance = get_overdue_balance_for_tenant(tenant.id)
-        tenant_usage_info = ["current_balance:" + str(curr_balance),
-                             "overdue_balance:" + str(overdue_balance)]
+        tenant_usage_info = [{"current_balance": curr_balance,
+                              "overdue_balance": overdue_balance}]
         return tenant_usage_info
     except (ObjectDoesNotExist):
         return Exception("Enter a valid ID")
--- django-project/tenant/tests/test_services.py
+++ django-project/tenant/tests/test_services.py
@@ -62,8 +62,8 @@ class TenantServicesTestCase(APITestCase):
         )
         self.tenant_charge1 = TenantCharge.objects.create(
             tenant=self.tenant1,
-            initial_amount=100,
-            remaining_amount=20,
+            initial_amount=100.0,
+            remaining_amount=20.0,
             description="Test Desc",
             bill_period_end_date=timezone.now().date() +
             timezone.timedelta(days=30),
@@ -74,8 +74,8 @@ class TenantServicesTestCase(APITestCase):
         )
         self.tenant_charge2 = TenantCharge.objects.create(
             tenant=self.tenant1,
-            initial_amount=100,
-            remaining_amount=25,
+            initial_amount=100.0,
+            remaining_amount=25.0,
             description="Test Desc",
             bill_period_end_date=timezone.now().date() +
             timezone.timedelta(days=30),
@@ -116,32 +116,32 @@ class TenantServicesTestCase(APITestCase):
         self.payment1 = Payment.objects.create(
             tenant=self.tenant1,
             payment_date=timezone.now().date() - timezone.timedelta(days=1),
-            payment_amount=100,
-            applied_amount=100,
+            payment_amount=100.0,
+            applied_amount=100.0,
             payment_method=self.payment_method,
         )
 
         self.payment2 = Payment.objects.create(
             tenant=self.tenant1,
             payment_date=timezone.now().date(),
-            payment_amount=50,
-            applied_amount=50,
+            payment_amount=50.0,
+            applied_amount=50.0,
             payment_method=self.payment_method,
         )
 
         self.payment3 = Payment.objects.create(
             tenant=self.tenant1,
             payment_date=timezone.now().date() - timezone.timedelta(days=30),
-            payment_amount=10,
-            applied_amount=10,
+            payment_amount=10.0,
+            applied_amount=10.0,
             payment_method=self.payment_method,
         )
 
         self.payment4 = Payment.objects.create(
             tenant=self.tenant2,
             payment_date=timezone.now().date(),
-            payment_amount=50,
-            applied_amount=50,
+            payment_amount=50.0,
+            applied_amount=50.0,
             payment_method=self.payment_method,
         )
 
@@ -153,15 +153,17 @@ class TenantServicesTestCase(APITestCase):
         self.assertIn(str(self.tenant1), actual)
         self.assertIn(str(self.tenant2), actual)
 
-    def test_fail_get_tenants_for_unit(self):
+    def test_fail_get_tenants_for_unit_id(self):
         """ Test case to fail get all tenants for a unit """
         unit_id1 = 0
-        unit_id2 = "s"
-        queryset1 = services.get_tenants_for_unit(unit_id1)
-        queryset2 = services.get_tenants_for_unit(unit_id2)
+        services.get_tenants_for_unit(unit_id1)
+        self.assertRaises(Exception, 'Enter a valid ID')
 
-        self.assertEqual(len(queryset1), 0)
-        self.assertFalse(queryset2)
+    def test_fail_get_tenants_for_unit_id_type(self):
+        """ Test case to fail get all tenants for a unit """
+        unit_id2 = "s"
+        services.get_tenants_for_unit(unit_id2)
+        self.assertRaises(Exception, 'Enter a numerical value for ID')
 
     def test_get_current_balance_for_tenant(self):
         """ Test case to get current balance for a tenant """
@@ -211,10 +213,10 @@ class TenantServicesTestCase(APITestCase):
         curr_bal2 = 201.0
         tenant_id1 = self.tenant1.id
         tenant_id2 = self.tenant2.id
-        usage1 = ["current_balance:" + str(curr_bal1),
-                  "overdue_balance:" + str(overdue_bal1)]
-        usage2 = ["current_balance:" + str(curr_bal2),
-                  "overdue_balance:" + str(overdue_bal2)]
+        usage1 = [{"current_balance": curr_bal1,
+                  "overdue_balance": overdue_bal1}]
+        usage2 = [{"current_balance": curr_bal2,
+                  "overdue_balance": overdue_bal2}]
         self.assertEqual(services.get_tenant_usage_info(tenant_id1),
                          usage1)
         self.assertEqual(services.get_tenant_usage_info(tenant_id2),
@@ -223,10 +225,7 @@ class TenantServicesTestCase(APITestCase):
     def test_fail_get_tenant_usage_info_id(self):
         """ Test fail when id entered does not exist """
         tenant_id1 = 0
-        # tenant_id2 = "s"
         services.get_tenant_usage_info(tenant_id1)
-        # usage1 = ["current_balance:" + str(0.0),
-        #           "overdue_balance:" + str(0.0)]
         self.assertRaises(Exception, 'Enter a valid ID')
 
     def test_fail_get_tenant_usage_info_id_type(self):
@@ -254,4 +253,26 @@ class TenantServicesTestCase(APITestCase):
         """Test fail when id is non integer"""
         tenant_id1 = "s"
         services.get_payments_for_tenant(tenant_id1)
+        self.assertRaises(Exception, 'Enter a numerical value for ID')
+
+    def test_get_charges_for_tenant(self):
+        """ Test to get all charges for a tenant """
+        tenant_id = self.tenant1.id
+        actual = services.get_charges_for_tenant(tenant_id)
+        actual = [str(actual[0]), str(actual[1])]
+        self.assertIn(str(self.tenant_charge1), actual)
+        self.assertIn(str(self.tenant_charge2), actual)
+        self.assertNotIn(str(self.tenant_charge3), actual)
+        self.assertNotIn(str(self.tenant_charge4), actual)
+
+    def test_fail_get_charges_for_tenant_id(self):
+        """ Test to fail get charges for tenant """
+        tenant_id1 = 0
+        services.get_charges_for_tenant(tenant_id1)
+        self.assertRaises(Exception, 'Enter a valid ID')
+
+    def test_fail_get_charges_for_tenant_id_type(self):
+        """ Test to fail get charges for tenant """
+        tenant_id2 = "s"
+        services.get_charges_for_tenant(tenant_id2)
         self.assertRaises(Exception, 'Enter a numerical value for ID')
